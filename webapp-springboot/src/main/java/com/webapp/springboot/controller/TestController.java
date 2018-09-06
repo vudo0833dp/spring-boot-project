ac97d7d039fe05cf8055aaf476e751f62000b2d4
vudo0833dp <hrdominhcaovu@gmail.com>
2018-09-07 01:20:02 +0700

    Add sql server config

--- webapp-springboot/src/main/java/com/webapp/springboot/controller/TestController.java
+++ webapp-springboot/src/main/java/com/webapp/springboot/controller/TestController.java
@@ -1,13 +1,28 @@
 package com.webapp.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.webapp.springboot.model.dummy.TestModel;
+import com.webapp.springboot.repository.TestRepository;
 
 @RestController
 public class TestController {
 
+	@Autowired
+	private TestRepository testRepository;
+	
 	@GetMapping(value = "/test")
 	public String test() {
 		return "Success";
+	}
+	
+	@GetMapping(value = "/data")
+	public TestModel getTestModel() {
+		TestModel testModel = new TestModel();
+		testModel.setName("Do Minh Cao Vu");
+		testModel.setAddress("CMT8");
+		return testRepository.save(testModel);
 	}
 }
--- webapp-springboot/src/main/java/com/webapp/springboot/launcher/ApplicationLauncher.java
+++ webapp-springboot/src/main/java/com/webapp/springboot/launcher/ApplicationLauncher.java
@@ -2,10 +2,14 @@ package com.webapp.springboot.launcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.webapp.springboot.*"})
+@EnableJpaRepositories(value = "com.webapp.springboot.repository")
+@EntityScan(value = "com.webapp.springboot.model.*")
 public class ApplicationLauncher {
 
 	public static void main(String[] args) {
